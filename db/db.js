5ffaf5b467654046e5df1c7538f6b5454ece7d74
Syddev <sydneysantos176@gmail.com>
2025-07-14 16:53:16 +0800

    fixed it back to local host

--- db/db.js
+++ db/db.js
@@ -5,19 +5,10 @@ dotenv.config();
 
 const connectToDb = async () => {
   try {
-    const uri = process.env.MONGO_URI;
-    if (!uri) {
-      throw new Error("❌ MONGO_URI not defined in environment variables");
-    }
-
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("✅ MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to DB ✅");
   } catch (error) {
-    console.log("❌ Error connecting to DB:", error.message);
+    console.log("Error in Connectin to DB ❌", error.message);
   }
 };
 
